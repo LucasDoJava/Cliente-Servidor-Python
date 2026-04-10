b41207d548284ad05652e0ab8536b4d440d5f92e
Lucas Emanuel <168912467+LucasDoJava@users.noreply.github.com>
2026-04-09 23:54:33 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,15 +1,14 @@
 import socket
 import threading
 
-
-class ClienteThread(threading.Thread):
+class LeituraThread(threading.Thread):
     def __init__(self, sock):
         super().__init__()
         self.sock = sock
 
     def run(self):
         try:
-            reader = self.sock.makefile("r")  
+            reader = self.sock.makefile("r")
 
             while True:
                 resposta = reader.readline()
@@ -18,12 +17,31 @@ class ClienteThread(threading.Thread):
                 print("Servidor:", resposta.strip())
 
         except Exception as e:
-            print("Erro na thread:", e)
+            print("Erro na leitura:", e)
+
+
+class EscritaThread(threading.Thread):
+    def __init__(self, sock):
+        super().__init__()
+        self.sock = sock
+
+    def run(self):
+        try:
+            while True:
+                msg = input()
+                self.sock.sendall((msg + "\n").encode())
+
+                if msg == "4":
+                    self.sock.close()
+                    break
+
+        except Exception as e:
+            print("Erro na escrita:", e)
 
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 6000))  # trocar pelo IP da rede wifi
+    sock.connect(("localhost", 6000))  # trocar pelo IP
 
     print("Conectado ao servidor!")
     print("1 - Hora")
@@ -31,21 +49,8 @@ def main():
     print("3 - Me fale algo legal")
     print("4 - Sair")
 
-    
-    thread = ClienteThread(sock)
-    thread.start()
-
-    try:
-        while True:
-            msg = input()
-            sock.sendall((msg + "\n").encode())  
-
-            if msg == "4":
-                break
-    except Exception as e:
-        print("Erro:", e)
-
-    sock.close()
+    LeituraThread(sock).start()
+    EscritaThread(sock).start()
 
 
 if __name__ == "__main__":
